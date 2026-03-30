97ec15da286aa1ae236d0260ad01f08eb065e894
Juan Oliveira <ruantos@proton.me>
2026-03-30 14:32:18 -0300

    feat: setting environment

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "indent": ["error", 2]
+    }
+  }
+];
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,20 @@
-import Fastify from 'fastify';
+// import Fastify from "fastify";
 
-const app = Fastify({
-  logger: true
-});
+// const app = Fastify({
+//   logger: true
+// });
 
-app.get('/', (request, reply) => {
-  reply.send({
-    message: "Mai é inteligente"
-  })
-})
+console.log('oi')
 
-app.listen({
-  port: 3000,
-  host: "localhost"
-}).then(() => {
-  console.log("HTTP server running...")
-})
+// app.get("/", (request, reply) => {
+//   reply.send({
+//     message: "Mai é inteligente"
+//   });
+// });
+
+// app.listen({
+//   port: 3000,
+//   host: "localhost"
+// }).then(() => {
+//   console.log("HTTP server running...");
+// });
